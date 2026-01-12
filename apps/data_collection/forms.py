af5abb4df824058c11ea914afa66d1280aaa7fc2
surajantre7-alt <surajantre7@gmail.com>
2026-01-12 09:48:14 +0530

    clean code

--- apps/data_collection/forms.py
+++ apps/data_collection/forms.py
@@ -5,7 +5,6 @@ from .models import (
     MonthlyDataCollection,
     DataCollectionResponse,
     DataCollectionQuestion,
-    DataCollectionCategory,
     DataCollectionAttachment,
     DataCollectionComment
 )
