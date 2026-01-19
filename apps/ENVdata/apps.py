43f9cb064867e9e393fcefca339e17d7fa364e37
Samarth Bhagane <samarthbhagane@gmail.com>
2026-01-19 17:34:21 +0530

    updated questions in data collection

--- apps/ENVdata/apps.py
+++ apps/ENVdata/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class EnvdataConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.ENVdata'
+    def ready(self):
+        from .seed import seed_environmental_questions
+        seed_environmental_questions()
