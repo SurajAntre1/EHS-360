7a0ff186ee1990ab006e86556bb689a21c68479b
surajantre7-alt <surajantre7@gmail.com>
2026-01-31 11:58:10 +0530

    profile and aligment

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -315,7 +315,7 @@ class UserDeleteView(LoginRequiredMixin, AdminRequiredMixin, DeleteView):
         return super().delete(request, *args, **kwargs)
 
 
-class UserDetailView(LoginRequiredMixin, AdminRequiredMixin, TemplateView):
+class UserDetailView(LoginRequiredMixin, TemplateView):
     """View user details - Only accessible by Admin"""
     template_name = 'accounts/user_detail.html'
     
