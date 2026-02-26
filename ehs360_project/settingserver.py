85411a8afada969e680dc6bfb6de49366fefe953
Samarth Bhagane <samarthbhagane@gmail.com>
2026-02-26 14:40:39 +0530

    Updated action-item-update form

--- ehs360_project/settingserver.py
+++ ehs360_project/settingserver.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1yy-9q3&$2o=&ozgx(%cdm^4-54jumb+)vy(=_nej!8jq_a(xr'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -193,7 +193,7 @@ EMAIL_USE_TLS = True               # Enable TLS
 EMAIL_USE_SSL = False 
 EMAIL_PORT = 587 
 EMAIL_HOST_USER = 'ehs.communication@everestind.com'
-EMAIL_HOST_PASSWORD = 'eil1012#'
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = 'EHS-360 System <ehs.communication@everestind.com>'
 
 
